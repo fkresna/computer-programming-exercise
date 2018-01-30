b16237d79dc95169ba17a2a5ac35fe855ee493b4
Freddy <fkresna@gmail.com>
2018-01-30 21:48:06 +0000

    add Traffic light python3

---
+++ python3/TrafficLight.py
@@ -0,0 +1,7 @@
+light = input("Please input the light(red, yellow, and green)")
+if(light == "red"):
+	print("Stop")
+elif (light == "yellow"):
+	print("Stand By")
+elif(light == "green"):
+	print("Run")
